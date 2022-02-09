643e6fe02f6c85594485af7421bd21ba0b5a239c
Marc Wynter <marcaws@amazon.com>
2022-02-09 11:36:41 -0500

    fix: local yaml path

--- dashboard/create-dashboards.sh
+++ dashboard/create-dashboards.sh
@@ -4,9 +4,9 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-templateURL="https://amazon-chime-sa-tools.s3.amazonaws.com/dashboard/aws-chime-vc-template-singledashboard.yaml"
+templateBody="file://aws-chime-vc-template-singledashboard.yaml"
 for vc in $(aws chime list-voice-connectors --query VoiceConnectors[].VoiceConnectorId --output text); do 
     region=$(aws chime list-voice-connectors --query "VoiceConnectors[?VoiceConnectorId=='$vc'].AwsRegion" --output text);
     echo "Creating vc-dashboard-$vc ..."
-    aws cloudformation create-stack --stack-name "vc-dashboard-$vc" --template-url $templateURL --parameters ParameterKey=VoiceConnectorId,ParameterValue=$vc ParameterKey=Region,ParameterValue=$region
+    aws cloudformation create-stack --stack-name "vc-dashboard-$vc" --template-body $templateBody --parameters ParameterKey=VoiceConnectorId,ParameterValue=$vc ParameterKey=Region,ParameterValue=$region
 done
